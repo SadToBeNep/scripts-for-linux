f41dfb129227e52980be0dbff15e505eaf30c54a
SadToBeNep <egocentricnep@gmail.com>
2024-07-29 11:20:40 +0200

    Some explanation for the vault_watcher script

--- vault_watcher.py
+++ vault_watcher.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+
+### This script was made so I can put it in some kind of bar, and it would display that if my luksVault is mounted or not
+### The script works by checking if there is a file availble or not, this file called CHECK is just an empty file that is
+### only used for this purpose, if it's not available display nothing in order to make the bar look more clean
 import os
 
 def luksWatcher():
